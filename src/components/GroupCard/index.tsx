5b5e5a54d2f4db395467b256074f1ff29187966c
Bruno <bvaleiro@gmail.com>
2023-04-13 11:02:50 -0300

    feature: add flatlist

--- src/components/GroupCard/index.tsx
+++ src/components/GroupCard/index.tsx
@@ -1,9 +1,11 @@
+
 import { TouchableOpacityProps } from 'react-native'
 import * as S from "./styles";
 type GroupCardProps = TouchableOpacityProps & {
   title: string
 }
 export function GroupCard({ title, ...rest }: GroupCardProps) {
+
   return (
     <S.ContainerGroupCard {...rest}>
       <S.Icon />
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,23 @@
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highligh } from '@components/Highlight';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import * as S from './styles';
 
 export function Group() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocketseat', "nova turma"])
   return (
     <S.ContainerGroup>
       <Header />
       <Highligh title='Turmas' subTitle='jogue com seus amigos' />
-      <GroupCard title="Jogue com seus amigos" />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+      />
     </S.ContainerGroup>
   );
 }
