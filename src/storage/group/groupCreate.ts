00273ea7af704f85a65c5f667df18b0f72746442
Bruno <bvaleiro@gmail.com>
2023-04-17 18:42:59 -0300

    feature: class erros handlings

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,10 +1,15 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { GROUP_COLECTION } from "@storage/storageConfig"
 import { groupGetAll } from "./gropuGetAll"
+import { AppError } from "@utils/AppError"
 
 export async  function groupCreate(newGroupName:string){
   try {
     const  storedGroups= await groupGetAll()
+    const groupAlreadyExists = storedGroups.includes(newGroupName)
+    if(groupAlreadyExists){
+    throw new AppError("Este grupo ja existe! Tente outro nome")
+    }
      await AsyncStorage.setItem(GROUP_COLECTION,JSON.stringify([...storedGroups,newGroupName]))
   } catch (error) {
     throw error
